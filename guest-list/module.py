78010a6ee4896629d43623d798cebe9ebc19c688
xjason141 <hamdi.zecl@gmail.com>
2023-12-13 00:53:27 +0800

    Failed to put guest info into a txt file. Try again later

--- guest-list/module.py
+++ guest-list/module.py
@@ -1,9 +1,10 @@
 #create class for guests
 
 class Guests: #class name must always start with a capital letter
-    def __init__(self, date, time, name, plate, reason):
+    def __init__(self, date, time, name, id, plate, reason):
         self.date = date
         self.time = time
         self.name = name
+        self.id = id
         self.plate = plate
         self.reason = reason
--- guest-list/register.py
+++ guest-list/register.py
@@ -7,14 +7,21 @@ import datetime
 def people():
     print("Please register here")
 
-    #guests info here (date, time, plate, name, reason)
+    #guests info here (date, time, plate, name, id, reason)
     guest = Guests(
         date=input('Date: '),
         time=input('Time: '),
-        plate=input('Plate: '),
         name=input('Name: '),
+        id=input('ID: '),
+        plate=input('Plate: '),
         reason=input('Reason: '),
         )
+
     return f'Welcome {guest.name} u sucker. You arrived here at {guest.time} on {guest.date} for {guest.reason}. Your vehicle number is {guest.plate}'
+
 print(people())
+
+
+
+
     
