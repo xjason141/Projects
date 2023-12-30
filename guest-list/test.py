8b283ab3ed90979e7156f9f191381b19f0266ba6
xjason141 <hamdi.zecl@gmail.com>
2023-12-30 18:29:12 +0800

    2 funcs. one for initial launch. 2nd for updating csv file. have not tested

--- guest-list/test.py
+++ guest-list/test.py
@@ -2,7 +2,7 @@
 from module import Guests
 import csv
 
-def people():
+def initial(): #need only to be launched once the first time to create a new csv file
     print("Please register here")
 
     #guests info here (date, time, plate, name, id, reason)
@@ -19,16 +19,40 @@ def people():
     insert = guest.asdict()
 
     #transfer guest info to place.csv
-
-    with open('place.csv', 'a+', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=insert.keys())
+    with open('place.csv', 'a', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=insert)
         writer.writeheader()
-        writer.writerow(insert)
+        writer.writerows(insert)
 
     file.close()
 
-    
-    
     return f'Welcome {guest.name} u sucker. You arrived here at {guest.time} on {guest.date} for {guest.reason}. Your vehicle number is {guest.plate}'
 
-print(people())
+print(initial())
+
+
+def updater():
+    print("Please register here")
+
+    #guests info here (date, time, plate, name, id, reason)
+    guest = Guests(
+        date=input('Date: '),
+        time=input('Time: '),
+        name=input('Name: '),
+        id=input('ID: '),
+        plate=input('Plate: '),
+        reason=input('Reason: '),
+        )
+
+    #transform guest info into a dict
+    insert = guest.asdict()
+
+    #transfer guest info to place.csv
+    with open('place.csv', 'a', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=insert)
+        writer.writeheader()
+        writer.writerows(insert)
+
+    file.close()
+
+    return f'Welcome {guest.name} u sucker. You arrived here at {guest.time} on {guest.date} for {guest.reason}. Your vehicle number is {guest.plate}'
