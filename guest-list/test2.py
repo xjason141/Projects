fdc4373f5af94f87a4bd47c946c52bc37a78a426
2D <pronoob141@gmail.com>
2024-01-15 22:32:55 +0800

    need to figure out how to use input in checking

--- guest-list/test2.py
+++ guest-list/test2.py
@@ -1,7 +1,6 @@
 #to get rows from csv
 import csv
 
-head = []
 check = []
 
 with open('guest-list/place.csv', 'r') as file:
@@ -10,43 +9,20 @@ with open('guest-list/place.csv', 'r') as file:
     read = csv.reader(file)
 
     #this syntax to skip the header in csv
-    head = next(file)
+    next(file)
 
     #append the rows in csv into a list
     for stuff in read:
         check.append(stuff)
+
     
     #checking
-    if 'Dodo' in str(check):
-        print('test complete')
+    if 'Didi' in str(check):
+        print(check[0][0], check[0][2]) #get value from check using index
     else:
         print('error')
 
-#code below does not work. needs fixing
-def create():
-    if exist == False:
-        initial()
-        print('file created. guest has been added')
-    else:
-        with open('guest-list/place.csv', 'r') as file:
-    
-        #reader object
-        read = csv.reader(file)
-
-        #this syntax to skip the header in csv
-        head = next(file)
-
-        #append the rows in csv into a list
-        for stuff in read:
-            check.append(stuff)
-    
-        #checking
-        if guest.name() in str(check):
-            print(guest.name() + ',' + guest.id())
-        else:
-            #print('error')
-            updater()
-            print('csv updated')
+print(check)
 
 
 
