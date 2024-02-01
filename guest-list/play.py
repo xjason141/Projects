48f37d179a132ba8e381afaad639973d43ddae1e
2D <pronoob141@gmail.com>
2024-02-01 18:13:37 +0800

    trying to rework app flow. check play.py in testing branch

---
+++ guest-list/play.py
@@ -0,0 +1,17 @@
+#chceker func 1st
+from module import Guests
+import datetime
+import csv
+import os
+
+def checker():
+    id = input("ID: ")
+    #create input to check if id exist in csv.
+    #if exist, run update() func
+    #if not, run initial()
+    #checker() not yet completed. still in creation phase.
+
+
+    print('The ID is ' + id)
+
+checker()
