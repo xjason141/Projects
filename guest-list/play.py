c031e70121bd634dc0df46b030dd160c4b91a8db
2D <pronoob141@gmail.com>
2024-02-08 22:16:40 +0800

    1. new decision. checker func is is now a new sepatate func. open play.py to read more.

--- guest-list/play.py
+++ guest-list/play.py
@@ -1,4 +1,8 @@
-#chceker func 1st
+#creating checker func.
+#checks if guest exists or not in csv file.
+#if guest exists, return/print entire info on guest.
+#checker() not yet completed. still in creation phase.
+
 from module import Guests
 import datetime
 import csv
@@ -7,7 +11,8 @@ import os
 curr_time = datetime.datetime.now().strftime('%H:%M')
 curr_date = datetime.datetime.now().strftime('%d-%b-%Y')
 
-def initial(): #run this if csv file does not exist
+
+def initial():
     print("Please register here")
 
     #guests info here (date, time, plate, name, id, reason)
@@ -22,27 +27,24 @@ def initial(): #run this if csv file does not exist
 
     print('success')
 
-
-
+#checker func
 def checker():
     id = input("ID: ").capitalize()
-    #create input to check if id exist in csv.
-    #if exist, run update() func
-    #if not, run initial()
-    #checker() not yet completed. still in creation phase.
 
     exist = []
+    x = dict(exist)
     with open('guest-list/place.csv') as file:
         reader = csv.reader(file, delimiter=',')
 
-        next(reader) # this syntax skips the header
+        next(reader) #tskips the header
 
         for row in reader:
             exist.append(row)
     
-    if id in str(exist):
-        print('the id is in here')
+    if id in str((exist)):
+        print(x)
+        print(exist)
     else:
-        initial()
+        print('not here')
 
 checker()
