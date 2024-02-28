23df9a0ccd82f1a4e44e40a605a5604663a798a1
2D <pronoob141@gmail.com>
2024-02-28 14:49:09 +0800

    fixed run.py for text-process

--- text-process/run.py
+++ text-process/run.py
@@ -1,20 +1,14 @@
-#! /usr/bin/env python3
-
 import os
 import requests
 
-
-for files in os.listdir('/data/feedback/'):
-    n_dict = {}
+for files in os.listdir('data/feedback/'):
     n_keys = ['title', 'name', 'date', 'feedback']
     count = 0
-    with open('/data/feedback/' + files, 'r') as file:
+    n_dict = {}
+    with open('data/feedback/' + files, 'r') as file:
         for lines in file:
             n_dict[n_keys[count]] = lines.rstrip()
             count += 1
-    print(n_dict)
+    # print(n_dict)
     response = requests.post('http://<corpweb-external-IP>/feedback', json=n_dict)
-    print(response.status_code)
-            
-
-
+print(response.status_code)
