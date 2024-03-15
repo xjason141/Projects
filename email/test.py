e50d5fbeac132fa3594fc791b95ecbbd007520c8
2D <pronoob141@gmail.com>
2024-03-15 16:54:30 +0800

    still trying to understand the solution

--- email/test.py
+++ email/test.py
@@ -1,14 +1,11 @@
 import cars
 import locale
+import json
 
 
-def calc():
-    max_revenue = {"revenue": 0}
-    x = cars.load_data('email/car_sales.json')
-    for vehicles in x:
-        for y in vehicles:
-            print(type(y))
-
-calc()
+"""Loads the contents of filename as a JSON file."""
+with open('email/car_sales.json') as things:
+    item_sales = things['total_sales']
+    print(item_sales)
 
     
