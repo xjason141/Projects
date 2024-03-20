4d8e69213e1d1713f3d0a9124ebc2dfb678d1b48
2D <pronoob141@gmail.com>
2024-03-20 17:26:03 +0800

    TODO max sales complete. Next is most popular car_year.

--- email/test.py
+++ email/test.py
@@ -6,12 +6,16 @@ import json
 """Loads the contents of filename as a JSON file."""
 with open('email/car_sales.json') as f:
     data = json.load(f)
-    newl = []
-    for cars in data:
-        newl.append(cars['total_sales'])
-    newl.sort()
-    print(newl)
-        
+    newlist = []
+    for item in data:
+        newlist.append(item['total_sales'])
+    newlist.sort()
+    x = newlist[-1]
+    
+    for item in data:
+        if item['total_sales'] == x:
+            most = item['car']['car_model']
+            print(most)
 
   
     # for stuff in data:
