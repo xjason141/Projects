6ddb47cedee7ac9b85a6265149b68b96054ad5eb
2D <pronoob141@gmail.com>
2024-03-23 09:44:48 +0800

    getting diff results between test.py and netexample.py

--- email/test.py
+++ email/test.py
@@ -1,6 +1,7 @@
 import cars
 import locale
 import json
+from operator import attrgetter
 
 #get the year with the most total_sales
 
@@ -12,4 +13,29 @@ with open('email/car_sales.json') as f:
     for item in data:
         item_year = item["car"]["car_year"]
         sales_by_year[item_year] = sales_by_year.setdefault(item_year, 0) + item['total_sales']
-    print(sales_by_year)
+    
+    most_popular_year = sorted(sales_by_year, key=sales_by_year.get, reverse=True)[0]
+    most_popular_sales = sales_by_year[item_year]
+    print(most_popular_year)
+    print(most_popular_sales)
+
+# newd = {'stud':{
+#     'name': 'didi',
+#     'score': 10
+# },
+# 'stud':{
+#     'name': 'dodo',
+#     'score': 50
+# },
+# 'stud': {
+#     'name': 'didi',
+#     'score': 30
+# },
+# 'stud': 'dodo',
+# 'score': 20
+# } 
+# #didi=40, dodo=70
+
+# values = newd['stud']
+# print(sales_by_year)
+
