53cafc60a79d2226ac16f0cc6ff3bc1d96148bff
2D <pronoob141@gmail.com>
2024-03-25 09:44:56 +0800

    Note to self. Read test.py comments in email

--- email/test.py
+++ email/test.py
@@ -7,35 +7,43 @@ from operator import attrgetter
 
 
 """Loads the contents of filename as a JSON file."""
-with open('email/car_sales.json') as f:
-    data = json.load(f)
-    sales_by_year = {}
-    for item in data:
-        item_year = item["car"]["car_year"]
-        sales_by_year[item_year] = sales_by_year.setdefault(item_year, 0) + item['total_sales']
+# with open('email/car_sales.json') as f:
+#     data = json.load(f)
+#     sales_by_year = {}
+#     for item in data:
+#         item_year = item["car"]["car_year"]
+#         sales_by_year[item_year] = sales_by_year.setdefault(item_year, 0) + item['total_sales']
     
-    most_popular_year = sorted(sales_by_year, key=sales_by_year.get, reverse=True)[0]
-    most_popular_sales = sales_by_year[item_year]
-    print(most_popular_year)
-    print(most_popular_sales)
+#     most_popular_year = sorted(sales_by_year, key=sales_by_year.get, reverse=True)[0]
+#     most_popular_sales = sales_by_year[item_year]
+#     print(most_popular_year)
+#     print(most_popular_sales)
 
-# newd = {'stud':{
-#     'name': 'didi',
-#     'score': 10
-# },
-# 'stud':{
-#     'name': 'dodo',
-#     'score': 50
-# },
-# 'stud': {
-#     'name': 'didi',
-#     'score': 30
-# },
-# 'stud': 'dodo',
-# 'score': 20
-# } 
-# #didi=40, dodo=70
+#explanation for syntax above
+# 1. 'item_year' is used to store the car years.
+# 2. line 15: iterates over the total_sales of each year. setdefault() is so that each similar year has one placeholder. e.g For each similar year, their total_sales are added up.
+# 3. havent fully understood the sorted part
 
-# values = newd['stud']
-# print(sales_by_year)
 
+
+
+new_dict = [
+    {'name': 'didi',
+     'score': 50,},
+    {'name': 'koko',
+     'score': 40},
+    {'name': 'didi',
+     'score': 20},
+    {'name': 'koko',
+     'score': 40}
+]
+
+arranged =  {}
+for students in new_dict:
+    names = students['name']
+    arranged[names] = arranged.setdefault(names, 0) + students['score']
+
+has_sorted = sorted(arranged, key=arranged.get, reverse=True)[0]
+score_sorted = arranged[names]
+print(has_sorted)
+print(score_sorted)
