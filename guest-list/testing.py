6772fb7e3fac4ee2fd81787902a53f7ec6b5d798
2D <pronoob141@gmail.com>
2024-04-03 13:55:09 +0800

    Fixed retrieve function.

--- guest-list/testing.py
+++ guest-list/testing.py
@@ -1,18 +1,17 @@
 import json
 
 x = 'guest-list/guests.json'
-y = 'email/car_sales.json'
 
 
-with open(x, 'r') as file:
-    data = json.load(file)
-    guest_info = data['guests']
+def retrieve(pathfile):
+    with open(pathfile, 'r') as file:
+        data = json.load(file)
+        guest_data = data['guests']
+        x = input('Name: ').capitalize()
 
-    guest_id = input('ID: ')
-    for item in guest_info:
-        if guest_info == item['id']:
-            guest_id = item['id']
-    
-        getting = '{}\n{}\n{}\n{}\n{}'.format(item['name'], item['date'], item['time'], item['id'], item['plate'])
+        for guests in guest_data:
+            if x == guests['name']:
+                x = guests['name']
+                print('{}\n{}'.format(guests['id'], guests['date']))
 
-print(getting)
+retrieve(x)
