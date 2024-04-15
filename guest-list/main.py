ef68a93e66a9e2ade438fe9a4899ffb792e9aac3
2D <pronoob141@gmail.com>
2024-04-15 15:39:41 +0800

    fixed worflow for main.py

--- guest-list/main.py
+++ guest-list/main.py
@@ -25,7 +25,7 @@ def initial(filepath):
     y = {'guests': [Guests.asdict(guest)]}
     with open(filepath, 'w') as guest_json:
         json.dump(y, guest_json, indent=2)
-    # print(f'{guest.name} arrived at {guest.time} on {guest.date}')
+    print(f'{guest.name} arrived at {guest.time} on {guest.date} going to {guest.address}')
 
 
 #run this if json file exists 
@@ -56,21 +56,33 @@ def retrieve(pathfile):
     with open(pathfile, 'r') as file:
         data = json.load(file)
         guest_data = data['guests']
-        x = input('Name: ').capitalize()
+        x = int(input('ID: '))
 
         for guests in guest_data:
-            if x == guests['name']:
-                x = guests['name']
-                print('{}\n{}\n{}\n{}'.format(guests['date'], guests['name'], guests['id'], guests['plate']))
+            if x == guests['id']:
+                x = guests['id']
+                print('{}\n{}\n{}\n{}\n{}'.format(guests['date'], guests['name'], guests['id'], guests['plate'], guests['address']))
 
 
-def main(filepath):
+def options(filepath):
     if os.path.exists(filepath):
         updater(filepath)
         print('Updating file')
     else:
         initial(filepath)
         print('New file created')
+
+
+def main(filepath):
+    one = ('Opt 1: Update')
+    two = ('Opt 2: Retrieve')
+    print(one + ', ' + two)
+    decision = int(input("Press 1 or 2: "))
+
+    if decision == 1:
+        options(filepath)
+    else:
+        retrieve(filepath)
 
 
 filepath = 'guest-list/guests.json'
