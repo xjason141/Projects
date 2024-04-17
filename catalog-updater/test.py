8b3593b5db25b7b5e669eeadf4d7940b08451f91
2D <pronoob141@gmail.com>
2024-04-17 12:19:06 +0800

    removed .tiff images.

--- catalog-updater/test.py
+++ catalog-updater/test.py
@@ -1,7 +1,7 @@
 import os
 from PIL import Image
 
-img_path = 'catalog-updater/supplier-data/images/'
+img_path = 'catalog-updater/supplier-data/images'
 
 for files in os.listdir(img_path):
     if files.endswith('tiff'):
