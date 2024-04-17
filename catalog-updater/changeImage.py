f7d2a0977e53e5d210673f524f7f6f1324cf0b1e
2D <pronoob141@gmail.com>
2024-04-17 11:52:35 +0800

    & C:/Users/DiDi/AppData/Local/Microsoft/WindowsApps/python3.11.exe c:/Users/DiDi/Python/Projects/catalog-updater/changeImage.py

--- catalog-updater/changeImage.py
+++ catalog-updater/changeImage.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 
 import os
-import PIL
+from PIL import Image
 
 img_path = 'catalog-updater/supplier-data/images'
-new_dir = 'supplier-data/converted'
+new_dir = r'catalog-updater/supplier-data/converted/'
 
 
-# if not os.path.exists(new_dir):
-#     os.makedirs(new_dir)
+if not os.path.exists(new_dir):
+    os.makedirs(new_dir)
 
 
 for img in os.listdir(img_path):
     if (img.endswith('.tiff')):
-        print(img)
+        name = img[:-4]
+
+        with Image.open(img_path + '/' + img) as pics:
+            new_pic = pics.resize((600,500)).convert('RGB')
+            print('Processing ' + str(img) + '...')
+            new_pic.save(new_dir + name + '.jpeg')
