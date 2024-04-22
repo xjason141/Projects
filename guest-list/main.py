29fdf5c65952c06b230b140f920aecd2b23d73df
2D <pronoob141@gmail.com>
2024-04-22 18:48:34 +0800

    added exceptions in guest-list/main.py. needs some tweaking. please run to find out what.

--- guest-list/main.py
+++ guest-list/main.py
@@ -3,14 +3,13 @@ import datetime
 import json
 import os
 
-
 curr_time = datetime.datetime.now().strftime('%H:%M')
 curr_date = datetime.datetime.now().strftime('%d-%b-%Y')
-
 
 #initial start up if json file does not exist
 def initial(filepath):
     print("Please register here")
+
     try:
         guest = Guests(
             date=curr_date,
@@ -21,20 +20,22 @@ def initial(filepath):
             reason=input('Reason: ').capitalize(),
             address=input('Address: ')
             )
-        
-        #prepare to load into json
+        if len(str(guest.id)) != 12:
+            raise Exception
+        # prepare to load into json
         y = {'guests': [Guests.asdict(guest)]}
         with open(filepath, 'w') as guest_json:
             json.dump(y, guest_json, indent=2)
-        print(f'{guest.name} arrived at {guest.time} on {guest.date} going to {guest.address}')
-
-    except ValueError:
-        print('Invalid ID')
-
+            
+    except ValueError as error:
+        print(type(error).__name__ + ': Invalid ID.')
+    except Exception:
+        print('ID must include only 12 characters.')
 
 #run this if json file exists 
 def updater(filepath):     
     print("Please register here")
+
     try:
         guest = Guests(
             date=curr_date,
@@ -46,17 +47,21 @@ def updater(filepath):
             address=input('Address: ')
             )
         
+        if len(str(guest.id)) != 12:
+            raise Exception
+        
         y = Guests.asdict(guest)
-
         with open(filepath, 'r') as f:
             data = json.load(f)
             data['guests'].append(y)
 
         with open(filepath, 'w') as f:
             json.dump(data, f, indent=2)
-    except ValueError:
-        print('Invalid ID')
 
+    except ValueError as error:
+        print(type(error).__name__ + ': Invalid ID.')
+    except Exception:
+        print('ID must include only 12 characters.')
 
 #retrieve guest info from guests.json
 def retrieve(pathfile):
@@ -66,13 +71,17 @@ def retrieve(pathfile):
             guest_data = data['guests']
             x = int(input('ID: '))
 
+            if len(str(x)) != 12:
+                raise Exception
+
             for guests in guest_data:
                 if x == guests['id']:
                     x = guests['id']
                     print('Guest info:\n{}\n{}\n{}\n{}\n{}'.format(guests['date'], guests['name'], guests['id'], guests['plate'], guests['address']))
-        except ValueError:
-            print('Invalid ID')
-
+        except ValueError as error:
+            print(type(error).__name__ + ': Invalid ID.')
+        except Exception:
+            print('ID must include only 12 characters.')
 
 #create new json or update existing json
 def options(filepath):
@@ -84,7 +93,6 @@ def options(filepath):
         print('New file created.')
         print('Guest updated.')
 
-
 def main(filepath):
     one = ('Opt 1: Update')
     two = ('Opt 2: Retrieve')
@@ -95,7 +103,6 @@ def main(filepath):
         options(filepath)
     else:
         retrieve(filepath)
-
 
 filepath = 'guest-list/guests.json'
 if __name__ == '__main__':
