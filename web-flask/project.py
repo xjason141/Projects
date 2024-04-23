c975d3302c1890a902950e8c4256f128c25f5210
2D <pronoob141@gmail.com>
2024-04-23 16:07:41 +0800

    cannot extend base.html. need to find fix quick.

--- web-flask/project.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-#note to self: to run app, switch to app dir, type "python -m flask --app <appName> run"
-
-app = Flask("myapp")
-
-@app.route('/')
-def hello_world():
-    return 'Hello, Didi!'
