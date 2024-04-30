0ca6386dd239f4ba7755f64e1607caea3b6e4db2
2D <pronoob141@gmail.com>
2024-04-30 15:27:10 +0800

    tested adding css

--- web-diary/main.py
+++ web-diary/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,9 +6,27 @@ app = Flask(__name__)
 def home():
     return render_template('base.html')
 
-@app.route('/user/<username>')
-def user(username):
-    return render_template('user.html')
+@app.route('/owner')
+def user():
+    rqst = request.args
+    owner = rqst.get('name')
+    return render_template('user.html', name=owner)
+
+#return json
+@app.route('/json')
+def get_json():
+    return jsonify({'dict data in here'})
+
+#get json data/accept json request
+@app.route('/data')
+def get_data():
+    data = request.json
+    return jsonify(data)
+
+#redirect to home
+@app.route('/go-to-home')
+def go_home():
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
