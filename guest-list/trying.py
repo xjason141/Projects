462172f91881454c097a37eaeb45d36b2296c5ba
2D <pronoob141@gmail.com>
2024-05-02 21:29:04 +0800

    a bunch of changes in main.py and modded module.py. needs testing.

--- guest-list/trying.py
+++ guest-list/trying.py
@@ -1,6 +1,6 @@
 #bug fix has not been merged with main.py. PLEASE REFER NUMS.PY FIRST BEFORE MERGING.
 
-from module import Guests
+from test_module import Guests
 import datetime
 import json
 import os
@@ -16,6 +16,7 @@ curr_date = datetime.datetime.now().strftime('%d-%b-%Y')
 #     print('not int')
 
 # l = isinstance(int(y), int)
+
 
 def initial(filepath):
     print("Please register here")
@@ -44,43 +45,12 @@ def initial(filepath):
     # except Exception:
     #     print('ID must include only 12 characters.')
 
+def options(filepath):
+    if os.path.exists(filepath):
+        print('file exists')
+    else:
+        initial(filepath)
 
-def updater(filepath):
-    print("Please register here")
-    # try:
-    guest = Guests(
-            date=curr_date,
-            time=curr_time,
-            name=input('Name: ').capitalize(),
-            id=input('ID: ' ),
-            plate=input('Plate: ').upper(),
-            reason=input('Reason: ').capitalize(),
-            address=input('Address: ')
-            )
-        
-        #ensure that guest.id is int
-        # id_check = int(guest.id)
-        # if isinstance(id_check, int) == False:
-        #     raise ValueError
-
-        # if len(guest.id) != 12:
-        #     raise Exception
-    y = Guests.asdict(guest)
-
-    with open(filepath, 'r') as f:
-        data = json.load(f)
-        data['guests'].append(y)
-
-    with open(filepath, 'w') as f:
-        json.dump(data, f, indent=2)
-    print('Guest updated.')
-    # print(Guests.asdict(guest))
-
-    # except ValueError as error:
-    #     print(type(error).__name__ + ': Invalid ID.')
-    # except Exception:
-    #     print('ID must include only 12 characters.')
-
-filepath = 'guest-list/guests.json'
+filepath = 'guest-list/test_guests.json'
 if __name__ == '__main__':
-    updater(filepath)
+    options(filepath)
