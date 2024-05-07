135350dc6946213b6ad4b4e277b54524269814d6
2D <pronoob141@gmail.com>
2024-05-07 13:12:10 +0800

    need to learn to create login window for gui.

--- gui/main.py
+++ gui/main.py
@@ -16,9 +16,21 @@ root.config(bg='#31464c')
 def login():
     print('Login Successfull')
 
-frame = CTkFrame(master=root, width=300, height=150, corner_radius=30, bg_color='white')
-# frame.grid(column=0, row=1)
+def checkbox_event():
+    print("checkbox toggled, current value:", check_var.get())
+
+frame = CTkFrame(root, width=300, height=150, corner_radius=30, bg_color='white')
 frame.place(rely=0.5, relx=0.5, anchor=CENTER)
+# frame.grid(column=0, row=1)
+
+button = CTkButton(frame, width=30, height=20, text='Login', border_spacing=5)
+button.place(rely=0.5, relx=0.5, anchor=CENTER)
+
+check_var = StringVar(value="on")
+checkbox = CTkCheckBox(root, text="checking", command=checkbox_event,
+                                     variable=check_var, onvalue="on", offvalue="off")
+checkbox.pack(pady=50, padx=50)
+
 
 
 root.mainloop()
