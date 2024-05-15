97ac85b35b2c919e1c44bb9e83c9b9640b6dd71d
2D <pronoob141@gmail.com>
2024-05-15 14:20:56 +0800

    some cleanup.

--- catalog-updater/download_bucket_file.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-wget https://storage.googleapis.com/gwg-content/gic223/supplier-data.tar.gz
--- catalog-updater/example_upload.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import requests
-
-#Shows how a file can be uploaded using the Python requests module
-
-url = 'http://localhost/upload/'
-with open('/usr/share/apache2/icons/icon.sheet.png', 'rb') as opened:
-    r = requests.post(url, files={'file': opened})
