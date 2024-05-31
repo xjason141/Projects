4022993fe831698e414b37583ba607ff67eb2308
2d <hamdi.zecl@gmail.com>
2024-05-31 20:26:52 +0800

    added README.md for gui dir.

---
+++ gui/README.md
@@ -0,0 +1,3 @@
+1. Began learning using customtkinter in main.py.
+2. Due to certain issues in its libs and dependencies, switched to built-in tkinter instead.
+3. Current project is now carried out in test_tk.py
--- gui/test_tk.py
+++ gui/test_tk.py
@@ -4,33 +4,41 @@ from tkinter import *
 root = Tk()
 root.title('test app')
 root.minsize(1000,650)
+# root.configure(bg='#f2a9e8')
 # root.maxsize(1000,650)
 
-# welcome_text = Label(text='welcome',fg='grey', pady=100, font=('arial', 20)) #to change font size, must include font type
-# welcome_text.pack()
+
 
 #login background
-loginBg = Frame(root, width=700, height=350, bg='grey')
-loginBg.place(relx=0.5, rely=0.5, anchor='center')
+# loginBg = Frame(root, width=700, height=350)
+# loginBg.place(relx=0.5, rely=0.5, anchor='center')
 
 #login frame
-loginFrame = Frame(loginBg, width=100, height=100)
-loginFrame.pack(side='bottom')
+# loginFrame = Frame(root, width=100, height=100, bg='red')
+# loginFrame.pack(side='')
+
+#welcome text
+welcome_text = Label(root, text='Welcome')
+welcome_text.grid(column=0, row=0, columnspan=2)
 
 
 #username and entry box
-userLabel = Label(loginFrame, text='Username', padx= 10)
+userLabel = Label(root, text='Username', padx=10)
 username = StringVar()
-userEntry = Entry(loginFrame, textvariable=username)
+userEntry = Entry(root, textvariable=username)
 userLabel.grid(column=0, row=1)
 userEntry.grid(column=1, row=1)
 
 # #password and entry box
-passLabel = Label(loginFrame, text='password', pady=10)
+passLabel = Label(root, text='Password', pady=10)
 password = StringVar()
-passEntry = Entry(loginFrame, textvariable=password, show='*')
-passLabel.grid(column=0, row=5)
-passEntry.grid(column=1, row=5)
+passEntry = Entry(root, textvariable=password, show='*')
+passLabel.grid(column=0, row=2)
+passEntry.grid(column=1, row=2)
 
+#login button
+log_btn = Button(root, text='Login')
+log_btn.grid(column=0, row=3, columnspan=2)
 
-root.mainloop()
+if __name__ == '__main__':
+    root.mainloop()
