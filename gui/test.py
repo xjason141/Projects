4b2c049d9f4e22a1c81ea3a580d1a4dc30c031e2
2d <hamdi.zecl@gmail.com>
2024-06-13 02:28:48 +0800

    learned how to switch frames in gui/test.py. use similar concept in gui/app.py

--- gui/test.py
+++ gui/test.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from tkinter import *
 from tkinter import messagebox
 import sqlite3 as sq
@@ -7,48 +8,44 @@ root = Tk()
 root.title('test app')
 root.minsize(1000,650)
 root.configure(bg='#581845')
-root.rowconfigure(0, weight=1)
-root.columnconfigure(0, weight=1)
+root.grid_columnconfigure(0,weight=1)
+root.grid_rowconfigure(0, weight=1)
 
 
-def main():
 
-    bgframe1 = Frame(root, bg='blue')
-    bgframe1.grid(row=0, column=0, sticky=NSEW)
+def mainF():
+    mainFrame = Frame(root, bg='red')
+    mainFrame.grid(column=0, row=0, sticky=NSEW)
 
-    #mainWin
-    mainWin = Frame(bgframe1, bg='blue', width=500, height=500)
-    mainWin.place(relx=0.5, rely=0.5, anchor=CENTER)
+    logFrame = Frame(mainFrame)
+    logFrame.place(relx=0.5, rely=0.5, anchor=CENTER)
 
-    #mainWin label
-    mainLabel = Label(mainWin, text='This is mainWin', bg='red', fg='white')
-    mainLabel.grid(column=1, row=0)
+    logBtn = Button(logFrame, text='Login', fg='black', command=secF)
+    logBtn.grid(column=1, row=1)
 
-    #mainBtn
-    chgBtn = Button(mainWin, text='Change to second window', command=secondWin)
-    chgBtn.grid(column=1, row=1)
+    mainFrame.tkraise()
+
+def secF():
+    secFrame = Frame(root, bg='blue')
+    secFrame.grid(column=0, row=0, sticky=NSEW)
+
+    guestFrame = Frame(secFrame)
+    guestFrame.place(rely=0.5, relx=0.5, anchor=CENTER)
+
+    exitBtn = Button(guestFrame, text='Log Out', fg='black', command=mainF)
+    exitBtn.grid(column=1, row=1)
 
 
-def secondWin():
 
-    bgframe2 = Frame(root, bg='magenta')
-    bgframe2.grid(row=0, column=0, sticky=NSEW)
-    
-    #secWin
-    secWin = Frame(root, bg='magenta', width=500, height=500)
-    secWin.place(relx=0.5, rely=0.5, anchor=CENTER)
 
-    #secWin label
-    secLabel = Label(secWin, text='This is secWin', bg='yellow', fg='black')
-    secLabel.grid(column=1, row=0)
+    secFrame.tkraise()
 
-    #secBtn
-    secBtn = Button(secWin, text='Change to first window window', command=main)
-    secBtn.grid(column=1, row=1)
 
-    # secWin.tkraise()
 
-main()
+
+
+
+mainF()
 
 if __name__ == '__main__':
     root.mainloop()
