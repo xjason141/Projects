cf5a7f14b08004fd9247084700cbf3fd3efefdc0
DiDi <hamdi.zecl@gmail.com>
2024-11-04 12:37:26 +0800

    for guest-js, main register function is complete. continue to other functions.

--- guest-js/guest.js
+++ guest-js/guest.js
@@ -11,31 +11,63 @@ class Guest {
         this.address = address;
     };
 
+    // Make sure first letter of name is capitalized. Have not tested for more than one word/name.
     upperCase() {
         const ask = prompt("Name: ");
         const first = ask[0].toUpperCase();
         return first + ask.substring(1);
-
     };
 
+    // transform info above into object, pushed into array
+    toDict() {
+        const toJson = [];
+        const values = [this.date, this.time, this.name, this.id, this.plate, this.reason, this.address];
+        const [The_Date, Time, Name, ID, Plate, Reason, Address] = values;
+        const dicted = {The_Date, Time, Name, ID, Plate, Reason, Address}
+        toJson.push(dicted);
+        console.log(`Guest ${Name} arrived at ${Time} on ${The_Date}.`);
+    };
+
+    // IdCheck
+    idCheck(guest_id) {
+        const id_check = parseFloat(guest_id);
+        if (id_check == false) {
+            throw new Error('wrong');
+        } else if (id_check.toString().length != 12) {
+            // console.log(id_check.toString().length);
+            throw new Error('Id too short');
+        };
+    };
+
+    // Main register function
     register() {
-        const currDate = new Date();
-        const whichAmPm = (currDate.getHours() >= 12) ? 'PM':'AM';
+        try{
+            const currDate = new Date();
+            const whichAmPm = (currDate.getHours() >= 12) ? 'PM':'AM';
+            
+            console.log("Please register here.");
+    
+            this.date = `${currDate.getDate()}/${currDate.getMonth() + 1}/${currDate.getFullYear()}`;
+            this.time = `${currDate.getHours()}:${currDate.getMinutes()} ${whichAmPm}`;
+            this.name = this.upperCase();
+            this.id = prompt("ID: ");
+            this.plate = prompt("Plate: ").toUpperCase();
+            this.reason = prompt("Reason: ");
+            this.address = prompt("Address: ");
+            
+            // check if id is valid
+            this.idCheck(this.id);
 
-        console.log("Please register here.");
+            // if valid, data transformed into obj
+            this.toDict();
 
-        this.date = `${currDate.getDate()}/${currDate.getMonth() + 1}/${currDate.getFullYear()}`;
-        this.time = `${currDate.getHours()}:${currDate.getMinutes()} ${whichAmPm}`;
-        this.name = this.upperCase();
-        this.id = prompt("ID: ");
-        this.plate = prompt("Plate: ").toUpperCase();
-        this.reason = prompt("Reason: ");
-        this.address = prompt("Address: ");
+        } catch (error) {
+            console.log(error)
+        }
 
-        console.log(`Guest ${this.name} has been registered on ${this.date} at ${this.time}.`);
+        // console.log(`Guest ${this.name} has been registered on ${this.date} at ${this.time}.`);
+        // console.log(toJson[0].Reason)
 
-        // const id_check = parseFloat(this.id);
-        // if (id_check == false || id_check.length != 12 ? "ID should be numbers only.": "ID too long or too short.") 
 
         // console.log(this.name);
     }
